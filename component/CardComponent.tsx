a17dc45551f8c925c02d1ffb76b7f048a0487db6
CristianHourcade [goiar] <hourcadecristian@gmail.com>
2021-06-08 15:18:15 -0300

    Testing config

--- component/CardComponent.tsx
+++ component/CardComponent.tsx
@@ -1,9 +1,10 @@
-import styles from "../styles/Home.module.css";
-
 export const CardComponent = ({ character }) => {
+  if(!character){
+    return <h4>Loading</h4>;
+  }
   return (
     <a
-      className={styles.card}
+      className={'card'}
     >
       <img src={character.image} style={{ width: "100%" }} />
       <h2>{character.name}</h2>
